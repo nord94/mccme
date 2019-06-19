82d84dfa29f2aeeb89492957c6f6b1a3b847ef6a
Vasilii <kiriushinvasilii@gmail.com>
2019-06-19 19:48:28 +0300

    mccme repository created

---
+++ src/searchandsort/binarysearch/FirstTask.java
@@ -0,0 +1,12 @@
+package searchandsort.binarysearch;
+
+import java.util.Scanner;
+
+public class FirstTask {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int k = input.nextInt();
+
+    }
+}
