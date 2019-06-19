f6f4a3f9916f0c856db72c77f990c7f216c9c802
Vasilii <kiriushinvasilii@gmail.com>
2019-06-19 20:30:47 +0300

    task01 done

---
+++ src/searchandsort/binarysearch/linsort/task01/LinSearch1.java
@@ -0,0 +1,26 @@
+package searchandsort.binarysearch.linsort.task01;
+
+import java.util.Scanner;
+
+public class LinSearch1 {
+    static int linSearch (int[] a, int k){
+        int num =0;
+        for (int i =0; i<a.length;i++){
+            if (a[i]==k) num++;
+        }
+    return num;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n=0;
+        n=input.nextInt();
+        int[] arr = new int[n];
+        int key;
+        for (int j=0;j<n;j++) {
+            arr[j]=input.nextInt();
+        }
+        key = input.nextInt();
+        System.out.println(linSearch(arr, key));
+    }
+}
