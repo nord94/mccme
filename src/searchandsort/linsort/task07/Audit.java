a42d4a87e354b508318adffff30b497f14a963a2
Vasilii <kiriushinvasilii@gmail.com>
2019-06-21 18:30:14 +0300

    Task07v2 done

--- src/searchandsort/linsort/task07/Audit.java
+++ src/searchandsort/linsort/task07/Audit.java
@@ -4,33 +4,37 @@ import java.util.Scanner;
 
 public class Audit {
     static Scanner input = new Scanner(System.in);
-    static int searchMin(int[] a){
-        int min;
-        min =0;
 
-        for (int i=0;i<a.length;i++){
-            if (a[i]<a[min]) min=i;
-        }
-        return min;
-    }
-    static int searchExMin(int[] a, int min){
+    static int[] searchMin(int[] a) {
+        int min;
         int exMin;
-        exMin = 0;
-        for (int i=0;i<a.length;i++){
-            if ((a[i]<a[exMin])&(a[i]!=a[min])) exMin=i;
+        int[] b = new int[2];
+        min = a[0] < a[1] ? 0 : 1;
+        exMin = min == 0 ? 1 : 0;
+        for (int i = 2; i < a.length; i++) {
+            if (a[i] < a[exMin]) {
+                if (a[i] < a[min]) {
+                    int j;
+                    j = min;
+                    min = i;
+                    exMin = j;
+                } else exMin = i;
+            }
         }
-        return exMin;
+        b[0] = min;
+        b[1] = exMin;
+        return b;
     }
 
     public static void main(String[] args) {
         int size;
         int[] array;
-        int min;
+        int min[];
 
         size = input.nextInt();
         array = new int[size];
         for (int i = 0; i < size; i++) array[i] = input.nextInt();
-        min=searchMin(array);
-        System.out.printf("%d %d", array[min], array[searchExMin(array, array[min])]);
+        min = searchMin(array);
+        System.out.printf("%d %d", array[min[0]], array[min[1]]);
     }
 }
