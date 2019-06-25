7dc2ef2b37a113a06e9fc3b6ee0a24e255ba0133
Vasilii <kiriushinvasilii@gmail.com>
2019-06-25 19:16:19 +0300

    BinSearch works, but only on one test

--- src/searchandsort/binsearch/first/BinSearch1.java
+++ src/searchandsort/binsearch/first/BinSearch1.java
@@ -6,24 +6,24 @@ public class BinSearch1 {
     static Scanner input = new Scanner(System.in);
 
     static int[] readValues(int size) {
-        int a[] = new int[size];
-        for (int i = 0; i < size; i++) {
-            a[i] = input.nextInt();
+        int array[] = new int[size];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = input.nextInt();
         }
-        return a;
+        return array;
     }
 
-    static int binSearch(int[] a, int b) {
+    static int binSearch(int[] array, int index) {
         int left;
         int right;
         int middle;
 
         left = 0;
-        right = a.length;
+        right = array.length;
 
         while (left < right) {
             middle = (left + right) / 2;
-            if (a[middle] - b < 0) left = middle + 1;
+            if (array[middle] - index < 0) left = middle + 1;
             else right = middle;
         }
         return right;
@@ -34,24 +34,26 @@ public class BinSearch1 {
         int k;
         int[] firstArray;
         int[] secondArray;
-        int closest;
-        int diff1;
-        int diff2;
+        int rightClosest;
+        int leftClosest;
+        int rightDiff;
+        int leftDiff;
 
-        diff2=0;
         n = input.nextInt();
         k = input.nextInt();
         firstArray = readValues(n);
         secondArray = readValues(k);
-        closest = 0;
+        rightClosest = 0;
+        leftClosest = 0;
         for (int i = 0; i < secondArray.length; i++) {
-            closest = firstArray[binSearch(firstArray, secondArray[i])];
-            diff1=secondArray[i]-closest;
-            if (i>0) diff2 = secondArray[i]-firstArray[binSearch(firstArray, secondArray[i])-1];
-            if (diff1==diff2){
-                System.out.println(firstArray[binSearch(firstArray, secondArray[i])-1]);
-            }
+            rightClosest = binSearch(firstArray, secondArray[i]);
+            leftClosest = rightClosest - 1;
+            rightDiff = Math.abs(secondArray[i] - firstArray[rightClosest]);
+            if (rightClosest > 0) leftDiff = Math.abs(secondArray[i] - firstArray[leftClosest]);
+            else leftDiff = -1;
+            if (leftDiff == rightDiff) {
+                System.out.println(firstArray[leftClosest]);
+            } else System.out.println(firstArray[rightClosest]);
         }
-
     }
 }
